5ec46ef201001f6fd515aeb2789adea39be67a55
Ville Saalo <ville.saalo@iki.fi>
2019-05-17 13:07:55 +0300

    Test that Hue values are stored into Influx on every run

---
+++ src/test/java/io/github/zeroone3010/hue2influx/Hue2InfluxTest.java
@@ -0,0 +1,60 @@
+package io.github.zeroone3010.hue2influx;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+class Hue2InfluxTest {
+  @Test
+  void testThatAllHueSamplesAreStoredToInflux() {
+    final HueService hueService = mock(HueService.class);
+    final InfluxService influxService = mock(InfluxService.class);
+    final ServiceFactory serviceFactory = new ServiceFactory() {
+      @Override
+      public HueService hueService() {
+        return hueService;
+      }
+
+      @Override
+      public InfluxService influxService() {
+        return influxService;
+      }
+    };
+
+    final Map<String, Double> sample1 = new HashMap<>();
+    sample1.put("Living room", 50d);
+    sample1.put("Kitchen", 100d);
+    final Map<String, Double> sample2 = new HashMap<>();
+    sample2.put("Living room", 50d);
+    sample2.put("Kitchen", 100d);
+    final Map<String, Double> sample3 = new HashMap<>();
+    sample3.put("Living room", 0d);
+    sample3.put("Kitchen", 100d);
+    final Map<String, Double> sample4 = new HashMap<>();
+    sample4.put("Living room", 0d);
+    sample4.put("Kitchen", 100d);
+    final Map<String, Double> sample5 = new HashMap<>();
+    sample5.put("Living room", 0d);
+    sample5.put("Kitchen", 0d);
+
+    when(hueService.getBrightnessByRoom()).thenReturn(sample1, sample2, sample3, sample4, sample5);
+    final InOrder influxInOrder = Mockito.inOrder(influxService);
+
+    final Hue2Influx hue2Influx = new Hue2Influx(serviceFactory);
+    hue2Influx.run();
+    influxInOrder.verify(influxService).store(sample1);
+    hue2Influx.run();
+    influxInOrder.verify(influxService).store(sample2);
+    hue2Influx.run();
+    influxInOrder.verify(influxService).store(sample3);
+    hue2Influx.run();
+    influxInOrder.verify(influxService).store(sample4);
+    hue2Influx.run();
+    influxInOrder.verify(influxService).store(sample5);
+  }
+}
